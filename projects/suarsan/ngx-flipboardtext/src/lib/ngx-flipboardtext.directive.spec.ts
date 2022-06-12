97307c27cd168c5982868ae853b134d780eca7bf
Javier Suárez <javiersuarsan@gmail.com>
2022-06-12 14:29:20 +0200

    feat - working with basic configuration

---
+++ projects/suarsan/ngx-flipboardtext/src/lib/ngx-flipboardtext.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NgxFlipboardtextDirective } from './ngx-flipboardtext.directive';
+
+describe('NgxFlipboardtextDirective', () => {
+  // it('should create an instance', () => {
+  //   const directive = new NgxFlipboardtextDirective();
+  //   expect(directive).toBeTruthy();
+  // });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxFlipboardtextModule } from 'projects/suarsan/ngx-flipboardtext/src/public-api';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgxFlipboardtextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
