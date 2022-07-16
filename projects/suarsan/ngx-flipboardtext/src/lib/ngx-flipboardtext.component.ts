259786e0aad8ddfa160e2571ed24cca47e65e08c
Javier Suárez <javiersuarsan@gmail.com>
2022-07-16 19:06:52 +0200

    test - add directive test
    Add ngx-fllipboardtext directive getAnimation() method test

--- projects/suarsan/ngx-flipboardtext/src/lib/ngx-flipboardtext.component.ts
+++ projects/suarsan/ngx-flipboardtext/src/lib/ngx-flipboardtext.component.ts
@@ -1,14 +1,14 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ngx-flipboardtext',
   templateUrl: 'ngx-flipboardtext.component.html',
   styleUrls: ['ngx-flipboardtext.component.scss']
 })
-export class NgxFlipboardtextComponent implements OnInit {
+export class NgxFlipboardtextComponent {
 
   @Input() keys!: Array<String>;
-  @Input() cycleDuration!: number;
+  @Input() cycleDuration: number;
   @Input() fontSize: String;
   @Input() breakRatio: number;
   @Input() backgroundColor: string;
@@ -17,16 +17,10 @@ export class NgxFlipboardtextComponent implements OnInit {
 
   constructor() {;
     this.fontSize = '2rem';
+    this.cycleDuration = 15000;
     this.breakRatio = 0.75;
     this.backgroundColor = '#2cc4d2';
     this.textColor = '#ffffff';
   }
-
-  ngOnInit(): void {
-  }
-  
-  ngAfterViewInit(): void {
-  } 
-  ngOnDestroy() {}
 
 }
--- projects/suarsan/ngx-flipboardtext/src/lib/ngx-flipboardtext.directive.ts
+++ projects/suarsan/ngx-flipboardtext/src/lib/ngx-flipboardtext.directive.ts
@@ -1,18 +1,22 @@
 import { animate, AnimationBuilder, AnimationFactory, AnimationMetadata, AnimationPlayer, AnimationStyleMetadata, keyframes, style } from '@angular/animations';
-import { OnInit, Directive, ElementRef, ChangeDetectorRef } from '@angular/core';
+import { OnInit, Directive, ElementRef } from '@angular/core';
 import { NgxFlipboardtextComponent } from './ngx-flipboardtext.component';
 
 @Directive({
-  selector: '[ngxFlipAnimationDirective]'
+  selector: '[ngxFlipboardtextDirective]'
 })
 export class NgxFlipboardtextDirective implements OnInit {
 
-  private player: AnimationPlayer;
-  private factory: AnimationFactory;
+  private player: AnimationPlayer | null = null;
+  private factory: AnimationFactory | null = null;
 
-  constructor(private eRef: ElementRef, private animationBuilder: AnimationBuilder, private ngxFlipboardtext: NgxFlipboardtextComponent) { 
-    this.factory= this.animationBuilder.build(this.getAnimation(this.ngxFlipboardtext));
-    this.player = this.factory.create(this.eRef.nativeElement);
+  constructor(private eRef: ElementRef, private ngxFlipboardtext: NgxFlipboardtextComponent, private animationBuilder?: AnimationBuilder,) { 
+    if (this.animationBuilder) {
+      this.factory = this.animationBuilder.build(this.getAnimation(this.ngxFlipboardtext));
+    }
+    if (this.factory) {
+      this.player = this.factory.create(this.eRef.nativeElement);
+    }
   }
   
   ngOnInit() {
@@ -20,28 +24,24 @@ export class NgxFlipboardtextDirective implements OnInit {
   }
   
   private play() {
-    this.player.restart();
-    this.player.onDone(() => this.play());
+    if (this.player) {
+      this.player.restart();
+      this.player.onDone(() => this.play());
+    }
   }
 
   private getAnimation(o: NgxFlipboardtextComponent): AnimationMetadata[] {
-    console.dir(o.breakRatio);
     const animationMetadata: AnimationMetadata[] = [];
     const frames: AnimationStyleMetadata[] = [];
     frames.push(style({ marginTop: `calc(0 * ${o.fontSize})`, offset: 0 }));
-    console.log(`calc(0 * ${o.fontSize})`, 0);
     frames.push(style({ marginTop: `calc(0 * ${o.fontSize})`, offset: (1 / o.keys.length) * o.breakRatio }));
-    console.log(`calc(0 * ${o.fontSize})`, (1 / o.keys.length) * o.breakRatio);
     for (let i = 0; i < o.keys.length; i++) {
       if ((i + 1) !== o.keys.length) {
         frames.push(style({ marginTop: `calc(-3 * ${i+1} * ${o.fontSize})`, offset: (i + 1) * 1 / o.keys.length }));
-        console.log(`calc(-3 * ${i+1} * ${o.fontSize})`, (i + 1) * 1 / o.keys.length);
         frames.push(style({ marginTop: `calc(-3 * ${i+1} * ${o.fontSize})`, offset: ((i + 1) * 1 / o.keys.length) + (1 / o.keys.length) * o.breakRatio }));
-        console.log(`calc(-3 * ${i+1} * ${o.fontSize})`, ((i + 1) * 1 / o.keys.length) + (1 / o.keys.length) * o.breakRatio );
       }
     }
     frames.push(style({ marginTop: `calc(-3 * ${o.keys.length} * ${o.fontSize})`, offset: 1 }));
-    console.log(`calc(-3 * ${o.keys.length} * ${o.fontSize})`, 1);
     animationMetadata.push(animate(`${o.cycleDuration}ms`, keyframes(frames)));
     return animationMetadata;
   }
